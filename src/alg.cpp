af9a0c0e511d03d247fe46185a7e7e7b08c5cd30
Малышева Галина <99182939+Malychwa@users.noreply.github.com>
2022-03-02 19:06:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ double pown(double value, uint16_t n) {
     return 1;}
   if (n == 1) {
     return value;}
-  return value*pown(value,n-1);
+  return value*pown(value, n-1);
 }
 
 uint64_t fact(uint16_t n) {
@@ -18,13 +18,13 @@ uint64_t fact(uint16_t n) {
 }
 
 double calcItem(double x, uint16_t n) {
-  return pown(x,n)/fact(n);
+  return pown(x, n)/fact(n);
 }
 
 double expn(double x, uint16_t count) {
   double e = 0;
   for (int n = 0; n <= count; n++) {
-    e += calcItem(x,n);
+    e += calcItem(x, n);
   }
   return e;
 }
@@ -32,7 +32,7 @@ double expn(double x, uint16_t count) {
 double sinn(double x, uint16_t count) {
   double s = 0;
   for (int n = 1; n <= count; n++) {
-    s += pown(-1,(n-1))*calcItem(x,(2*n-1));
+    s += pown(-1, (n-1))*calcItem(x, (2*n-1));
   }
   return s;
 }
@@ -40,7 +40,7 @@ double sinn(double x, uint16_t count) {
 double cosn(double x, uint16_t count) {
   double c = 0;
   for (int n = 1; n <= count; n++) {
-    c += pown(-1,(n-1))*calcItem(x,(2*n-2));
+    c += pown(-1, (n-1))*calcItem(x, (2*n-2));
   }
   return c;
 }
