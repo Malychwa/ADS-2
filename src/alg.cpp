79bf2e1bbedae3ffc9d981a131221e65d6627a55
Малышева Галина <99182939+Malychwa@users.noreply.github.com>
2022-03-02 18:20:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -23,15 +23,15 @@ double calcItem(double x, uint16_t n) {
 
 double expn(double x, uint16_t count) {
   double e = 0;
-  for (int i = 0; i < count; i++) {
-    e += calcItem(x,i);
+  for (int n = 0; n < count; n++) {
+    e += calcItem(x,n);
   }
   return e;
 }
 
 double sinn(double x, uint16_t count) {
   double s = 0;
-  for (int i = 1; i <= count; i++) {
+  for (int n = 1; n <= count; n++) {
     s += pown(-1,n-1)*calcItem(x,2n-1);
   }
   return s;
@@ -39,7 +39,7 @@ double sinn(double x, uint16_t count) {
 
 double cosn(double x, uint16_t count) {
   double c = 0;
-  for (int = 1; i <= count; i++) {
+  for (int n = 1; n <= count; n++) {
     c += pown(-1,n-1)*calcItem(x,2n-2);
   }
   return c;
